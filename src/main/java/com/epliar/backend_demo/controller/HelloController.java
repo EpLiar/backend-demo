2d7052b2256ae37ff79399d1eace9337b74ca44a
EpLiar <e@epliar.com>
2021-03-18 23:27:20 +0800

    init

---
+++ src/main/java/com/epliar/backend_demo/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.epliar.backend_demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("")
+    public static String sayHello() {
+        return "你好，世界！";
+    }
+}
