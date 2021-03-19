8a39e253bb2a3a1d39debc408119fe2f80443961
EpLiar <e@epliar.com>
2021-03-19 10:56:57 +0800

    Create runscript.sh

---
+++ runscript.sh
@@ -0,0 +1,9 @@
+PATH=/usr/bin:$PATH
+APP="backend_demo-0.0.1-SNAPSHOT.jar"
+mvn install
+PID=-1
+PID=$(ps -ef | grep $APP | grep -v grep | awk '{print $2}')
+if [[ ! $PID==-1 ]]; then
+  kill -l KILL $PID 
+fi
+BUILD_ID=dontKillMe nohup java -jar target/$APP &
