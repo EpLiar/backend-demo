0999a9d279dc23ea5628c7f93c6de6956e96258c
EpLiar <e@epliar.com>
2021-03-19 11:58:32 +0800

    Update runscript.sh

--- runscript.sh
+++ runscript.sh
@@ -1,11 +1,8 @@
 PATH=/usr/bin:$PATH
 APP="backend_demo-0.0.1-SNAPSHOT.jar"
 mvn install
-PID=-1
 PID=$(ps -ef | grep $APP | grep -v grep | awk '{print $2}')
-if [[ ! $PID==-1 ]]; then
-  kill -9 $PID 
-fi
+kill -9 $PID
 BUILD_ID=dontKillMe
 nohup java -jar target/$APP &
 sleep 5
