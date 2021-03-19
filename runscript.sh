19b4272c68c3073df3031db6412ad64dc142f15d
EpLiar <e@epliar.com>
2021-03-19 11:23:11 +0800

    Update runscript.sh

--- runscript.sh
+++ runscript.sh
@@ -1,6 +1,6 @@
 PATH=/usr/bin:$PATH
 APP="backend_demo-0.0.1-SNAPSHOT.jar"
-mvn install
+mvn clean install
 PID=-1
 PID=$(ps -ef | grep $APP | grep -v grep | awk '{print $2}')
 if [[ ! $PID==-1 ]]; then
